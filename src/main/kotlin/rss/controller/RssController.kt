d2a1a0c469f02fad84b925f81bd50504f54bf085
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-04-22 14:32:45 +0900

    feat: 10분마다 rss를 불러오는 기능 구현

--- src/main/kotlin/rss/controller/RssController.kt
+++ src/main/kotlin/rss/controller/RssController.kt
@@ -1,5 +1,10 @@
 package rss.controller
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import rss.model.BlogPosts
 import rss.model.RssParser
@@ -7,36 +12,58 @@ import rss.view.InputView
 import rss.view.OutputView
 
 class RssController {
-    private lateinit var blogPosts: BlogPosts
+    private var latestPosts = BlogPosts()
     private val rssParser = RssParser()
 
-    fun run() {
-        runBlocking {
-            blogPosts = rssParser.fetchAllBlogs(rssUrls)
-        }
+    fun run() = runBlocking {
+        latestPosts = rssParser.fetchAllBlogs(urls = rssUrls)
+        handleUserInput()
+        startPolling()
+    }
 
-        val keyword = InputView.inputBlogKeyword()
+    private fun CoroutineScope.handleUserInput() {
+        launch(Dispatchers.IO) {
+            while (isActive) {
+                val keyword = InputView.inputBlogKeyword()
 
-        if (keyword.isNullOrEmpty()) {
-            findAll()
-        } else {
-            findKeyword(keyword)
+                if (keyword.isNullOrBlank()) {
+                    showAllPosts()
+                } else {
+                    showKeywordSearch(keyword)
+                }
+            }
         }
     }
 
-    private fun findAll() {
-        val top10 = blogPosts.takeLatest(limit = 10)
+    private fun CoroutineScope.startPolling(pollingTime: Long = 60 * 10 * 1000L) =
+        launch {
+            var previousPosts = latestPosts
 
-        OutputView.showSearchBlog(top10)
+            while (isActive) {
+                latestPosts = rssParser.fetchAllBlogs()
+                val newPosts = latestPosts.list.minus(previousPosts.list.toSet())
+                if (newPosts.isNotEmpty()) {
+                    OutputView.showSearchBlog(newPosts)
+                }
+                previousPosts = latestPosts
+                delay(pollingTime)
+            }
+        }
+
+    private fun showAllPosts(limit: Int = 10) {
+        val topPosts = latestPosts.takeLatest(limit)
+        OutputView.showSearchBlog(topPosts)
     }
 
-    private fun findKeyword(keyword: String) {
-        val filtered = blogPosts.findKeywordLatest(keyword = keyword, limit = 10)
-
+    private fun showKeywordSearch(keyword: String, limit: Int = 10) {
+        val filtered = latestPosts.findKeywordLatest(keyword, limit)
         OutputView.showSearchBlog(filtered)
     }
 
     companion object {
-        private val rssUrls = setOf("https://techblog.woowahan.com/feed", "https://v2.velog.io/rss/skydoves/")
+        private val rssUrls = setOf(
+            "https://techblog.woowahan.com/feed",
+            "https://v2.velog.io/rss/skydoves/"
+        )
     }
 }
